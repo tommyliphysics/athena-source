0a1e11fc78eba33be036b44d9cc0169a46369d69
Tommy Li <tommyliphys@gmail.com>
2025-04-21 03:19:44 +1000

    local changes

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,16 +3,17 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/ClfReports', component: () => import('pages/ClfReports.vue') },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ]
 
 export default routes
